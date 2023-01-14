a99bf3a58d15a95738142407c7f79782ec4954ec
Jame Tran <jamedhtran@gmail.com>
2023-01-14 16:02:55 -0500

    first test

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-hi
+import os
+import openai
+
+openai.api_key = "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"
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Create promo copy for the FamilyTime mobile application. It allows unlimited uploading, special filters and makes it easy to create albums of photos and videos. It runs on iOS and Android:",
+  temperature=0.5,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+def generate_text(prompt):
+    prompt = "Create promo copy for the FamilyTime mobile application. It allows unlimited uploading, special filters and makes it easy to create albums of photos and videos. It runs on iOS and Android:"
+
+print(response.choices[0].text)
