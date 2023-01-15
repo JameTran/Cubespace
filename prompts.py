67d096f100a05aee5b03b8b9eb53a925a4f760c7
Jame Tran <jamedhtran@gmail.com>
2023-01-14 23:30:30 -0500

    added helper functions

--- prompts.py
+++ prompts.py
@@ -6,9 +6,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_text(prompt : str) -> str:
     response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.5,
-    max_tokens=64)
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=64
+    )
     return response.choices[0].text
 
+def generate_image(prompt : str) -> str:
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    #returns a URL
+    return response['data'][0]['url']
